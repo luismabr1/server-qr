02a166a957d3ca6060b31afb75651065fbd1bb0c
Luis Manuel Brito <lsbrt4@gmail.com>
2022-05-09 09:11:22 -0400

    estructura inicial de proyecto de api

---
+++ api/components/user/network.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const response = require('../../../network/response')
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    response.success(req, res, 'Todo Correcto', 200)
+})
+
+module.exports = router;
